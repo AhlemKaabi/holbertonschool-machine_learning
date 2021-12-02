21fb2d800f371eb2f346d0619a31a83810c5f240
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-02 18:30:31 +0100

    fix pycodestyle

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""
+    Concatenates two arrays.
+"""
+
 
 def cat_arrays(arr1, arr2):
+    """
+        Concatenates two arrays.
+    """
     cat_arr = []
 
     for elem1 in arr1:
