4abc5794f33b8bcbc96919ba6a1a31003cde0853
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-02 19:56:28 +0100

    fix pycodestyle

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+"""
+    matrix multiplication.
+"""
 
-import numpy as np
+
 def np_matmul(mat1, mat2):
+    """
+        matrix multiplication.
+    """
+    import numpy as np
+
     A = np.array(mat1)
     B = np.array(mat2)
     C = A.dot(B)
