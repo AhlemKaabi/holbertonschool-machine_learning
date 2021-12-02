77dc3a9bfb334b8f4bba7d478779ac677af32635
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-02 17:59:23 +0100

    fix pycodestyle

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-	Calculates the shape of a matrix.
+    Calculates the shape of a matrix.
 """
+
+
 def matrix_shape(matrix):
-	"""
-		Return the shape of a matrix.
-	"""
-	if not type(matrix) == list:
-		return []
-	return [len(matrix)] + matrix_shape(matrix[0])
+    """
+        Return the shape of a matrix.
+    """
+    if not type(matrix) == list:
+        return []
+    return [len(matrix)] + matrix_shape(matrix[0])
