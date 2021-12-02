cd8911c65465ffaa22a41558037295ba252764ed
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-02 18:05:50 +0100

    fix pycodestyle

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -2,25 +2,30 @@
 """
     Transpose of a 2D matrix.
 """
+
+
 def matrix_shape(matrix):
-	"""
-		Return the shape of a matrix.
-	"""
-	if not type(matrix) == list:
-		return []
-	return [len(matrix)] + matrix_shape(matrix[0])
+    """
+        Return the shape of a matrix.
+    """
+    if not type(matrix) == list:
+        return []
+    return [len(matrix)] + matrix_shape(matrix[0])
+
 
 def matrix_transpose(matrix):
+    """
+        Returns the transpose of a 2D matrix.
+    """
     size = matrix_shape(matrix)
     rows = size[1]
     colums = size[0]
-    transpose_matrix = [[0] * colums for i in range(rows)]
-    # return numpy.transpose(matrix).array
-    transpose_matrix_rows = 0
+    tran_matrix = [[0] * colums for i in range(rows)]
+    tran_matrix_ro = 0
     for row_matrix in matrix:
-        transpose_matrix_colums = 0
+        tran_matrix_col = 0
         for column_matrix_elem in row_matrix:
-            transpose_matrix[transpose_matrix_colums][transpose_matrix_rows] = column_matrix_elem
-            transpose_matrix_colums += 1
-        transpose_matrix_rows += 1
-    return transpose_matrix
+            tran_matrix[tran_matrix_col][tran_matrix_ro] = column_matrix_elem
+            tran_matrix_col += 1
+        tran_matrix_ro += 1
+    return tran_matrix
