caa3b343106a399d853e44893cd8027ff708efec
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-02 19:37:33 +0100

    fix pycodestyle

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
+"""
+    element-wise addition, subtraction,
+    multiplication, and division.
+"""
+
 
 def np_elementwise(mat1, mat2):
+    """
+        element-wise addition, subtraction,
+        multiplication, and division.
+    """
+    import numpy as np
+    # addition
+    add = np.array(np.add(mat1, mat2))
+    # subtraction
+    sub = np.array(np.subtract(mat1, mat2))
+    # multiplication
+    mul = np.array(np.multiply(mat1, mat2))
+    # division
+    div = np.array(np.divide(mat1, mat2))
+    res = (add, sub, mul, div)
+    return res
