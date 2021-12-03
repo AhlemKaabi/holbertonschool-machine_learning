dcca1ed7d2d9d4f3b28c23db521ee6779b7429b6
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-03 13:05:27 +0100

    fix more cases

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,19 +8,28 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
         Concatenates two matrices along a specific axis.
     """
+    match = 0
     # create a copy of mat1
     cp_mat1 = [[columns for columns in rows] for rows in mat1]
     # create a copy of mat2
     cp_mat2 = [[columns for columns in rows] for rows in mat2]
 
+
+
     # concatenate rows
     if axis == 0:
-        Res_cat_0 = []
-        for rows in cp_mat1:
-            Res_cat_0.append(rows)
-        for rows in cp_mat2:
-            Res_cat_0.append(rows)
-        return Res_cat_0
+        # check if we can concatenate the two matrices
+        for rows1, rows2 in zip(mat1, mat2):
+            if len(rows1) == len(rows2):
+                match = 1
+            break
+        if match == 1:
+            Res_cat_0 = []
+            for rows in cp_mat1:
+                Res_cat_0.append(rows)
+            for rows in cp_mat2:
+                Res_cat_0.append(rows)
+            return Res_cat_0
     # concatenate columns
     if axis == 1:
         Res_cat_1 = []
--- math/0x00-linear_algebra/7-main.py
+++ math/0x00-linear_algebra/7-main.py
@@ -1,16 +1,50 @@
 #!/usr/bin/env python3
 
-cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
+# cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
 
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-print(mat5)
-mat1[0] = [9, 10]
-mat1[1].append(5)
-print(mat1)
-print(mat4)
-print(mat5)
+# mat1 = [[1, 2], [3, 4]]
+# mat2 = [[5, 6]]
+# mat3 = [[7], [8]]
+# mat4 = cat_matrices2D(mat1, mat2)
+# mat5 = cat_matrices2D(mat1, mat3, axis=1)
+# print(mat4)
+# print(mat5)
+# mat1[0] = [9, 10]
+# mat1[1].append(5)
+# print(mat1)
+# print(mat4)
+# print(mat5)
+
+# import sys
+# cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
+# m1 = [[4, -7, 56, 2], [5, 106, 7, 2]]
+# m2 = [[2, -6, 3], [0, -6, 3]]
+# m = cat_matrices2D(m1, m2)
+# print(m)
+# m1 = [[484, 247], [554, 16], [5, 88]]
+# m2 = [[233, -644, 325], [406, -16, 33], [765, 34, -39]]
+# m = cat_matrices2D(m1, m2, axis=0)
+# print(m)
+# m1 = [[-54, -87, 95], [54, 16, -72]]
+# m2 = [[12, 63, 79], [-10, 69, -9], [76, 45, -11]]
+# m = cat_matrices2D(m1, m2, axis=1)
+# print(m)
+
+
+import sys
+cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
+m1 = [[51, 24, 73], [93, 45, 77]]
+m2 = [[], [], []]
+m = cat_matrices2D(m1, m2)
+print(m)
+m2 = [[51, 24, 73], [93, 45, 77]]
+m1 = [[], [], []]
+m = cat_matrices2D(m1, m2, axis=0)
+print(m)
+m1 = [[75, 23, 58], [32, 5, 67], [34, 65, 22]]
+m2 = [[], [], []]
+m = cat_matrices2D(m1, m2, axis=1)
+if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
+    print("Not a new matrix")
+    sys.exit(1)
+print(m)
