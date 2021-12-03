2d54612fb757fcfa51d52ccac8626560b9f9ea9c
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-03 13:28:32 +0100

    fix checker style

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -2,11 +2,11 @@
 """
     Concatenates two matrices along a specific axis.
 """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """
         Concatenates two matrices along a specific axis.
     """
-    import numpy as np
     return np.concatenate((mat1, mat2), axis)
