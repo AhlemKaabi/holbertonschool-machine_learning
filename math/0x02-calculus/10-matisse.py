1e1dd8bb3a62c25d89922e99fb17e38548d3ca17
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-08 14:47:36 +0100

    check style req

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-	Calculates the derivative of a polynomial.
+    Calculates the derivative of a polynomial.
 """
 
 
 def poly_derivative(poly):
     """
-    	Calculates the derivative of a polynomial.
+        Calculates the derivative of a polynomial.
     """
     deg = len(poly)
-    return [ i * poly[i] for i in range(deg) if i - 1 >= 0]
+    return [i * poly[i] for i in range(deg) if i - 1 >= 0]
