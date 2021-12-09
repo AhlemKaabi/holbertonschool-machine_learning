eb6c53c2a59d885d13f21c32796c5d2fe1b224c9
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-09 17:19:02 +0100

    slicing

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
-
+"""
+    Silce a numpy array with Dictionary of axes as keys
+    and slices as values
+"""
 import numpy as np
 def np_slice(matrix, axes={}):
-	new_array = np.array([])
+    """
+        matrix: is the matrix to slice
+        axes={axis number: slice,...}
+    """
+    # to slice a np.array we need a tuple of slices!
+    # matrix[tuple(slices)]
+    # slices should be ordered, first axis, second ...
+    mat_slices = []
+
+    for axis_num in range(matrix.ndim):
+        # get the right slice for axis_num
+        tuple_slice = axes.get(axis_num)
+        if tuple_slice is not None:
+            mat_slices.append(slice(*tuple_slice))
+        else:
+            mat_slices.append(slice(None))
+
+    return matrix[tuple(mat_slices)]
+
 
