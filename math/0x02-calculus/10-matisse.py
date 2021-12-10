612ff68ae3d539a25a5ae6385d6cfb2ad2488122
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-10 17:53:34 +0100

    0 derivative

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -9,6 +9,6 @@ def poly_derivative(poly):
         Calculates the derivative of a polynomial.
     """
     if poly == []:
-        return None
+        return [0]
     deg = len(poly)
     return [i * poly[i] for i in range(deg) if i - 1 >= 0]
