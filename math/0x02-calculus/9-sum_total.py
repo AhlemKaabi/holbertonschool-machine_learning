3050854d91064182f5ce9b0e0155411e2d588cd4
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-11 14:03:59 +0100

    fix case n is invalid

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -10,7 +10,7 @@ def summation_i_squared(n):
         that calculates Sum Sigma
     """
 
-    if n < 0:
+    if n < 0 or type(n) is not int:
         return None
     res = (n * (n + 1) * ((2 * n) + 1))/6
     return res
