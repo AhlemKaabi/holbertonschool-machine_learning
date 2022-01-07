63bd46207cce81358787de49cda46b3aae91efba
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-07 16:15:33 +0100

    starting with TF

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+	Creating a tensorflow Layer.
+"""
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """
+	Method:
+		Creating a layer.
+	@prev: is the tensor output of the previous layer
+	@n: is the number of nodes in the layer to create
+	@activation: is the activation function that the layer
+ 	should use
+    """
+
+    layer =
+
+    
