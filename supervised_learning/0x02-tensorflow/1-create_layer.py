a3a24663e4e58762030f939f71a3a391a1b22250
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-15 01:05:35 +0100

    create layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
 """
-	Creating a tensorflow Layer.
+    Creating a tensorflow Layer.
 """
 import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
     """
-	Method:
-		Creating a layer.
-	@prev: is the tensor output of the previous layer
-	@n: is the number of nodes in the layer to create
-	@activation: is the activation function that the layer
- 	should use
+    Method:
+        Creating a layer.
+    @prev: is the tensor output of the previous layer
+    @n: is the number of nodes in the layer to create
+    @activation: is the activation function that the layer
+     should use
     """
+    # https://keras.io/api/layers/core_layers/dense/
 
-    layer =
+    init_weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    
+    # https://www.tensorflow.org/guide/keras/sequential_model
+
+    layer = tf.keras.layers.Dense(n,
+                                  activation=activation,
+                                  kernel_initializer=init_weights,
+                                  name="layer")
+
+    Input_data = prev
+    return layer(Input_data)
