547fbc6841d416c1f3427410c48e0a0b9458ca94
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-21 22:47:16 +0100

    fix dropout matrices

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -5,6 +5,18 @@
 import numpy as np
 
 
+def dropout_matrices(weights, m, keep_prob, L):
+    np.random.seed(1)
+    D = {}
+
+    for l in range(L):
+        # initialize the random values for the dropout matrix
+        D[str(l)] = np.random.rand(weights['W', + str(l)].shape[1], m)
+        # Convert it to 0/1 to shut down neurons corresponding to each element
+        D[str(l)] = D[str(l)] < keep_prob[l]
+        assert(D[str(l)].shape == (weights['W', + str(l)].shape[1], m))
+    return D
+
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
     Method:
@@ -21,19 +33,25 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         a dictionary containing the outputs of each layer and
         the dropout mask used on each layer
     """
+    # get number of examples
+    m = X.shape[1]
     # https://tinyurl.com/56eyn35t
+    D = dropout_matrices(weights, m, keep_prob, L)
     output = {}
     A = X
-    output['A0'] = X
+    A = np.multiply(A, D[str(0)])
+    A /= keep_prob
+    output['A0'] = A
     for i in range(1, L + 1):
         Z = np.matmul(weights['W' + str(i)], A) + weights['b' + str(i)]
         if i == L:
             A = np.exp(Z)/np.sum(np.exp(Z))
         else:
             A = (np.exp(Z) - np.exp(-Z))/(np.exp(Z) + np.exp(-Z))
-        d = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(int)
-        A = np.multiply(A, d)
+        # d = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(int)
+        A = np.multiply(A, D[str(i)])
         A /= keep_prob
         output['A' + str(i)] = A
-        output['D' + str(i)] = d
+        output['D' + str(i)] = D[str(i)]
     return output
+
