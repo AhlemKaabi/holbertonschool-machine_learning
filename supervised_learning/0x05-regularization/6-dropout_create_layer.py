dcfdd1a29ce8e1b627181d9f33cd4f4a8a739057
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-24 23:46:43 +0100

    dropout layer

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
 """
-	L2 Regularization Cost
+    L2 Regularization Cost
 """
+import tensorflow.compat.v1 as tf
 
 
-def l2_reg_cost(cost, lambtha, weights, L, m):
+def dropout_create_layer(prev, n, activation, keep_prob):
     """
-	Method:
-		calculates the cost of a neural network with
-  		L2 regularization.
+    Method:
+        that creates a layer of a neural network using dropout.
 
     Parameters:
-		@cost:
-		@lambtha:
-		@weights:
-		@L:
-		@m:
+        @prev: tensor containing the output of the previous layer
+        @n: is the number of nodes the new layer should contain
+        @activation: is the activation function that should be
+            used on the layer
+        @keep_prob: is the probability that a node will be kept
 
-	Returns:
-		cost of the network accounting for L2 regularization
+    Returns:
+        the output of the new layer
+
     """
+    # https://www.tensorflow.org/api_docs/python/tf/keras/layers/Dropout
+    # class Dropout: Applies Dropout to the input.
+
+    init_weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    dropout = tf.keras.layers.Dropout(keep_prob)
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=init_weights,
+                            kernel_regularizer=dropout,
+                            name="layer")
+    # output = regularizer(layer(prev))
+    output = layer(prev)
+    return output
