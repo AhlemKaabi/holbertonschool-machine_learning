144eb3c146c4d80ffa2cf46501d36013ec3ee7e4
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-26 00:23:19 +0100

    Save and Load Weights

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    Keras - Save and Load Weights
+"""
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    Method:
+        saves a model's weights.
+
+    Parameters:
+        @network: the model whose weights should be saved
+        @filename: the path of the file that the weights
+         should be saved to
+        @save_format: the format in which the weights
+         should be saved
+
+    Returns: None
+    """
+    network.save_weights(filename)
+    return None
+
+
+def load_weights(network, filename):
+    """
+    Method:
+        loads a model's weights.
+
+     Parameters:
+        @network: the model to which the weights should be loaded
+        @filename is the path of the file that the weights
+        should be loaded from
+
+    Returns:
+         None
+
+    """
+    return network.load_weights(filename)
+
