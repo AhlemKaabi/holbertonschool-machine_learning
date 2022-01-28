ab136e3cb765362f669bb1d245719116280a4709
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-28 17:37:42 +0100

    evaluate a model importing keras

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -12,10 +12,10 @@ def test_model(network, data, labels, verbose=True):
 
     Parameters:
         @network: the network model to test.
-           @data: the input data to test the model with.
+        @data: the input data to test the model with.
         @labels: he correct one-hot labels of data.
         @verbose: boolean that determines if output should
-             be printed during the testing process.
+            be printed during the testing process.
 
     Returns:
          the loss and accuracy of the model with the
