b2b342c2347cc3f585aec2d1814260fea64f2b07
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-28 13:59:53 +0100

    correct the dropout rate

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -42,7 +42,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         hidden_layer = K.layers.Dense(layers[i],
                                       activation=activations[i],
                                       activity_regularizer=reg)(hidden_layer)
-        hidden_layer = K.layers.Dropout(1- keep_prob)(hidden_layer)
+        hidden_layer = K.layers.Dropout(1 - keep_prob)(hidden_layer)
 
     model = K.Model(inputs=inputs, outputs=outputs)
     return model
