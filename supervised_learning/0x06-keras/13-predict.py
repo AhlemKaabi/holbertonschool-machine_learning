4525e2fae1164617d299232c258118b9444ade2d
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-28 17:38:27 +0100

    predict

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+	Keras - Predict
+"""
+def predict(network, data, verbose=False):
+    """
+    Method:
+        makes a prediction using a neural network:.
+
+    Parameters:
+        @network: the network model to test.
+        @data: the input data to make the prediction with.
+        @verbose: boolean that determines if output should
+            be printed during the testing process.
+
+    Returns:
+        the prediction for the data
+    """
+    return network.predict(x=data, verbose=verbose)
