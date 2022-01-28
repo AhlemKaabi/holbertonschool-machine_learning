8d68c864af7aa7bd88258d383f53acc503fbbf4c
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-28 17:28:16 +0100

    evaluate a model

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Keras - Test
+"""
+def test_model(network, data, labels, verbose=True):
+    """
+    Method:
+        that tests a neural network.
+
+    Parameters:
+        @network: the network model to test.
+           @data: the input data to test the model with.
+        @labels: he correct one-hot labels of data.
+        @verbose: boolean that determines if output should
+             be printed during the testing process.
+
+    Returns:
+         the loss and accuracy of the model with the
+           testing data, respectively
+    """
+    # evaluate() :Returns the loss value & metrics values
+    # for the model in test mode.
+    return network.evaluate(x=data, y=labels, verbose=1)
