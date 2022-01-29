598cbc0a9612db46ee3c9c50fe000ca448ad4612
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-29 15:25:18 +0100

    working with strides

---
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+    Strided Convolution
+"""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Method:
+        performs a convolution on grayscale images.
+
+    @images(numpy.ndarray), shape (m, h, w)
+        containing multiple grayscale images.
+        - m: the number of images
+        - h: the height in pixels of the images
+        - w: the width in pixels of the images
+    @kernel(numpy.ndarray), shape (kh, kw)
+        containing the kernel for the convolution
+        - kh: the height of the kernel
+        - kw: the width of the kernel
+    @padding: tuple of (ph, pw)
+        - ph: the padding for the height of the image
+        - pw: the padding for the width of the image
+
+     @stride: tuple of (sh, sw)
+        - sh: the stride for the height of the image
+        - sw: the stride for the width of the image
+
+    Returns:
+        a numpy.ndarray containing the convolved images.
+    """
+    # only allowed to use two for loops;
+    # any other loops of any kind are not allowed
+    # input_width and input_height
+    m, input_h, input_w = images.shape
+
+    # flip the kernal matrix
+    # kernel = np.transpose(kernel) (already!)
+    # kernel_width and kernel_height
+    kernel_h, kernel_w = kernel.shape
+    sh, sw = stride[0], stride[1]
+    if padding == 'valid':
+        ph = pw = 0
+    elif padding == "same" :
+        ph = int(np.ceil( (input_h - 1) * sh + (kernel_h - input_h) / 2))
+        pw = int(np.ceil( (input_w - 1) * sw + (kernel_w - input_w) / 2))
+    else:
+        ph = padding[0]
+        pw = padding[1]
+
+    output_height = int(((input_h + 2 * ph - kernel_h) / stride[0]) + 1)
+    output_width = int(((input_w + 2 * pw - kernel_w) / stride[1]) + 1)
+
+    # Same convolution output
+    output = np.zeros((m, output_height, output_width))
+
+
+    # Add zero padding to the input image
+    image_padded = np.pad(images,
+                        pad_width=((0, 0), (ph, ph), (pw, pw)),
+                        mode='constant')
+
+    # Loop over every pixel of the output
+    for i in range(output_height):
+        for j in range(output_width):
+            x = i * sh
+            y = j * sh
+            # element-wise multiplication of the kernel and the image
+            img_slice = image_padded[:, x:x+kernel_h, y:y+kernel_w]
+            output[:, i, j] = np.tensordot(img_slice, kernel)
+    return output
