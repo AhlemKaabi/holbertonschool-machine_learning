6c9b7d310d5bb67f221bed5c46bd0b1017731adf
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-30 16:04:13 +0100

    edit initializer + pycodestyle

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -33,7 +33,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
 
     init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
                                                          mode=("fan_avg"))
-    
+
     regularizer = tf.keras.regularizers.L2(lambtha)
     layer = tf.layers.Dense(n, activation=activation,
                             kernel_initializer=init_weights,
