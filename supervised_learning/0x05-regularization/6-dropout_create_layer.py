6e038a28aae8b216679e512f14a0f9d101d39914
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-30 14:27:23 +0100

    adding the scale arg

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -25,7 +25,7 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     # class Dropout: Applies Dropout to the input!(layer, weights..)
     # kernel_regularizer: Regularizer to apply a penalty on the layer's kernel
     #   -> Tries to reduce the weights W (excluding bias)
-    init_weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    init_weights = tf.keras.initializers.VarianceScaling(scale=1.0, mode='fan_avg')
 
     dropout = tf.layers.Dropout(keep_prob)
     layer = tf.layers.Dense(n, activation=activation,
