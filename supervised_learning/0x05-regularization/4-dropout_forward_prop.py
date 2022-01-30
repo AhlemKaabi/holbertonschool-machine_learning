de6b4b86a4b547b6c7797b43c063b7185619df8c
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-30 16:23:59 +0100

    edit softmax formula

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -58,11 +58,11 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     for i in range(1, L + 1):
         Z = np.matmul(weights['W' + str(i)], A) + weights['b' + str(i)]
         if i == L:
-            A = np.exp(Z)/np.sum(np.exp(Z))
+            A = np.exp(Z)/np.sum(np.exp(Z), axis=0, keepdims=True)
         else:
             A = (np.exp(Z) - np.exp(-Z))/(np.exp(Z) + np.exp(-Z))
             A = np.multiply(A, D[str(i)])
-            A /= 1- keep_prob
+            A /= keep_prob
             output['D' + str(i)] = D[str(i)]
         output['A' + str(i)] = A
     return output
