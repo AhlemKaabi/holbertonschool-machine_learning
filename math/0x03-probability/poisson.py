3f72dc424e89dd9f903106d000a170589798f9ab
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-04 14:06:59 +0100

    updates 1- if data not else 2- fix pycodestyle

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -4,22 +4,27 @@
     Class Poisson that represents a poisson distribution:
 """
 
+
 class Poisson:
     def __init__(self, data=None, lambtha=1.):
         """
             Args:
-                data: list of the data to be used to estimate the distribution.
-                lambtha: the expected number of occurences in a given time frame.
+                data: list of the data to be used to estimate
+                the distribution.
+                lambtha: the expected number of occurences
+                in a given time frame.
+
             Raises:
                 ValueError: If size is less than 0
         """
         self.lambtha = float(lambtha)
-        if data is None :
+
+        if data is None:
             if (lambtha <= 0):
                 raise ValueError("lambtha must be a positive value")
-        if data:
+        else:
             if (type(data) is not list):
                 raise TypeError("data must be a list")
             if (len(data) < 2):
-                 raise ValueError("data must contain multiple values")
+                raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
