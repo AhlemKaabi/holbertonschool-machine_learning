62e4a70534bfc3cb8639d41040bba959b0c2980f
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-05 00:10:59 +0100

    updates

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
     Class Poisson that represents a poisson distribution.
 """
@@ -7,9 +6,11 @@
 
 class Poisson:
     """
-    Class Poisson that represents a poisson distribution.
+        Class Poisson that represents a poisson distribution.
     """
-    # https://www.sciencedirect.com/topics/mathematics/poisson-distribution
+
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """
             Args:
@@ -23,36 +24,16 @@ class Poisson:
                 TypeError: data must be a list
                 ValueError: data must contain multiple values
         """
-        self.lambtha = float(lambtha)
-        self.pi = 3.1415926536
-        self.e = 2.7182818285
-
         if data is None:
-            if (lambtha <= 0):
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
         else:
-            if (type(data) is not list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            if (len(data) < 2):
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
-
-    def fact(self, n):
-        """
-        Method:
-            calculates factorial of given number
-
-        Args:
-            @n: number
-
-        Returns:
-            factorial of given number
-        """
-
-        if n == 1 or n == 0:
-            return 1
-        else:
-            return n * self.fact(n - 1)
+            self.lambtha = sum(data) / len(data)
 
     def pmf(self, k):
         """
@@ -66,14 +47,21 @@ class Poisson:
         Returns:
             the PMF value for k
         """
+        def fact(n):
+            """
+                calculates factorial of given number
+            """
+            if n == 1 or n == 0:
+                return 1
+            else:
+                return n * fact(n-1)
         k = int(k)
         if k < 0:
             return 0
-        k_ = self.fact(k)
-        return ((self.e ** -self.lambtha) * (self.lambtha ** k)) / k_
+        return (self.lambtha ** k * self.e ** -self.lambtha) / fact(k)
 
     def cdf(self, k):
-        """
+        """"
         Method:
             Calculates the value of the CDF for
             a given number of “successes”
@@ -87,7 +75,8 @@ class Poisson:
         k = int(k)
         if k <= 0:
             return 0
+
         cdf = 0
         for k in range(1, k + 1):
             cdf += self.pmf(k)
-        return cdf
+        return
