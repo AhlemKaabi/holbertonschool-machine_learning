60b33934fa5f8708d59c143b6a00599fc13f43d1
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-04 14:35:06 +0100

    if data is given

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 """
-    Class Poisson that represents a poisson distribution:
+    Class Poisson that represents a poisson distribution.
 """
 
 
 class Poisson:
+    """
+    Class Poisson that represents a poisson distribution.
+    """
     def __init__(self, data=None, lambtha=1.):
         """
             Args:
@@ -15,16 +18,17 @@ class Poisson:
                 in a given time frame.
 
             Raises:
-                ValueError: If size is less than 0
+                ValueError: lambtha must be a positive value
+                TypeError: data must be a list
+                ValueError: data must contain multiple values
         """
         self.lambtha = float(lambtha)
 
         if data is None:
             if (lambtha <= 0):
                 raise ValueError("lambtha must be a positive value")
-        else:
-            if (type(data) is not list):
-                raise TypeError("data must be a list")
-            if (len(data) < 2):
-                raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+        if (type(data) is not list):
+            raise TypeError("data must be a list")
+        if (len(data) < 2):
+            raise ValueError("data must contain multiple values")
+        self.lambtha = float(sum(data) / len(data))
