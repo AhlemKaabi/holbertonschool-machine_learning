a2fe571901fafab2fe1f268980c75c51e36411df
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-06 20:20:39 +0100

    check batch norm

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -16,16 +16,13 @@ def create_batch_norm_layer(prev, n, activation):
         @n is the number of nodes in the layer to be created
         @activation is the activation function that should be
         used on the output of the layer
-        you should use the tf.keras.layers.Dense layer as the base layer with kernal initializer tf.keras.initializers.VarianceScaling(mode='fan_avg')
-        your layer should incorporate two trainable parameters, gamma and beta, initialized as vectors of 1 and 0 respectively
-        you should use an epsilon of 1e-8
 
     Returns:
         a tensor of the activated output for the layer
     """
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    layer = tf.keras.layers.Dense(units=n, kernel_initializer = initializer)
+    layer = tf.keras.layers.Dense(units=n, kernel_initializer=initializer)
 
     # Calculate the mean and variance of layer
     # for simple batch normalization pass axes=[0] (batch only).
