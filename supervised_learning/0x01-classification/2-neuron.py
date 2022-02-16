b388161590030bea6332433f7c64aed37b8393e5
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-16 18:13:28 +0100

    update sigmoid function

--- supervised_learning/0x01-classification/2-neuron.py
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
 """
- class Neuron that defines a single neuron
- performing binary classification.
+     Class Neuron that defines a single neuron
+     performing binary classification.
 """
 import numpy as np
 
 
 class Neuron:
     """
-        class Neuron.
+        Class Neuron.
     """
     def __init__(self, nx):
         """
-            Args:
-                nx : is the number of input features to the neuron
-                W : The weights vector for the neuron (random normal
-                distribution).
-                b : The bias for the neuron.
-                A : The activated output of the neuron (prediction)
+        Method:
+            Constructor
+        Args:
+            nx : is the number of input features to the neuron
+            W : The weights vector for the neuron (random normal
+            distribution).
+            b : The bias for the neuron.
+            A : The activated output of the neuron (prediction)
         """
         if type(nx) is not int:
-            raise TypeError("nx must be n integer")
+            raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.__nx = nx
-        self.__W = np.random.normal(size=(1, 784))
+        # size=(1, nx) because we have one neuron (one unit)
+        self.__W = np.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
 
+    # bundling data with the methods that operate on them.
+    # getter for retrieving the data
+    # the setter for changing the data. (we don't have stter here!)
     @property
     def W(self):
         """get the weights
@@ -54,13 +59,16 @@ class Neuron:
 
     def forward_prop(self, X):
         """
+        Method:
             Calculates the forward propagation of the neuron
-            Args:
-                X (numpy.ndarray): shape (nx, m)
-                    nx : he number of input features to the neuron
-                    m : the number of examples
-          """
+		Args:
+			X (numpy.ndarray): shape (nx, m)
+				nx : he number of input features to the neuron
+				m : the number of examples
+		Returns:
+			Returns the private attribute __A
+        """
         z = (self.W * self.A) + self.b
         # new A using sigmoid activation function
-        self.__A = (1 + np.exp(-z))
+        self.__A = 1 / (1 + np.exp(-z))
         return self.A
