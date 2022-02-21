01a6e744c209dfaa31d49bd4e678e6a4e1f52fec
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-21 20:20:59 +0100

    neural_network

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -33,9 +33,9 @@ class NeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if type(nodes) is not int:
-            raise TypeError("nx must be an integer")
-        if nodes < 0:
-            raise ValueError("nx must be a positive integer")
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.normal(size=(nodes, nx))
         self.W2 = np.random.normal(size=(1, nodes))
