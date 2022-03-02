2a5cf64f357724561201d1e4f8a3f11b54813786
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-03-02 15:37:24 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -28,17 +28,12 @@ def one_hot_encode(Y, classes):
     if not isinstance(Y, np.ndarray):
         return None
 
-
-
+    # classes is larger than largest element in Y
+    # classes is smaller than largest element in Y
     # https://www.kite.com/python/answers/how-to-do-one-hot-encoding-with-numpy-in-python
-    try:
-        data = np.array(Y)
-        shape = (data.size, data.max() + 1)
-        one_hot = np.zeros(shape)
-        rows = np.arange(data.size)
-        one_hot[rows, data] = 1
-        return one_hot.T
-    except Exception:
-        # classes is larger than largest element in Y
-        # classes is smaller than largest element in Y
-        return None
+    data = np.array(Y)
+    shape = (data.size, data.max() + 1)
+    one_hot = np.zeros(shape)
+    rows = np.arange(data.size)
+    one_hot[rows, data] = 1
+    return one_hot.T
