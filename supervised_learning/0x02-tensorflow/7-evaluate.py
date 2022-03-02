e961222875b0259e6559d2a106aaa6e14a67a266
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-03-02 21:53:54 +0100

    remove comments

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -30,20 +30,12 @@ def evaluate(X, Y, save_path):
         saver = tf.train.import_meta_graph(save_path + ".meta")
 
         saver.restore(session, save_path)
-        # graph = tf.get_default_graph()
-        # x = graph.get_operation_by_name('x').outputs[0]
-        # y = graph.get_operation_by_name('y').outputs[0]
 
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
-
         y_pred = tf.get_collection('y_pred')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
-
-    #     y_pred = graph.get_operation_by_name('y_pred').outputs[0]
-    #     accuracy = graph.get_operation_by_name('accuracy').outputs[0]
-    #     loss = graph.get_operation_by_name('loss').outputs[0]
 
         prediction = session.run(y_pred, feed_dict={x: X, y: Y})
         acc_eval = session.run(accuracy, feed_dict={x: X, y: Y})
