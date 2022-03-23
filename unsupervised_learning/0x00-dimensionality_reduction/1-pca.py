037fb354f6e084e0d93912f2b7407be17b87fc3e
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-03-23 13:54:14 +0100

    PCA2

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    PCA -  Principal Component Analysis
+"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Method:
+        Performs PCA on a dataset
+
+    Args:
+        X[numpy.ndarray], shape (n, d):
+        - n: the number of data points
+        - d: the number of dimensions in each point
+        ndim: The new dimensionality of the transformed X.
+
+    Returns:
+        T[numpy.ndarray], shape (n, ndim)
+        containing the transformed version of X
+    """
+    # We will calculate the W (Eigendecomposition) using the SVD
+    # without Computing the (X.transpose X) covariance matrix
+    # SVD: Singular Value Decomposition
+
+    _, _, vh = np.linalg.svd(X)
+
+    W = vh.T[:, :ndim]
+    return X.dot(W)
