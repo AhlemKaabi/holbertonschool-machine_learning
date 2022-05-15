f4d7283676df2c0c02677e66bc5afd57be4233ec
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-05-15 16:52:31 +0100

    use gensim.models.keyedvectors method

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -2,7 +2,7 @@
 """
     Extract Word2Vec
 """
-from tensorflow.keras.layers import Embedding
+# from tensorflow.keras.layers import Embedding
 
 
 # https://github.com/RaRe-Technologies/gensim/wiki/Using-Gensim-Embeddings-with-Keras-and-Tensorflow
@@ -19,15 +19,19 @@ def gensim_to_keras(model):
     trainable keras Embedding
 
     """
-    keyed_vectors = model.wv
-    # structure holding the result of training
-    weights = keyed_vectors.vectors
-    # vectors themselves, a 2D numpy array
+    # keyed_vectors = model.wv
+    # # structure holding the result of training
+    # weights = keyed_vectors.vectors
+    # # vectors themselves, a 2D numpy array
 
-    layer = Embedding(
-        input_dim=weights.shape[0],
-        output_dim=weights.shape[1],
-        weights=[weights],
-        trainable=False,
-    )
-    return layer
+    # layer = Embedding(
+    #     input_dim=weights.shape[0],
+    #     output_dim=weights.shape[1],
+    #     weights=[weights],
+    #     trainable=False,
+    # )
+
+    # return layer
+
+    # https://notebook.community/piskvorky/gensim/docs/notebooks/keras_wrapper
+    return model.wv.get_keras_embedding(True)
