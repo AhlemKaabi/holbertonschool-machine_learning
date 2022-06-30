1b7b7bdaf61449696bcd0610fed23aa98d1211f2
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-06-30 20:33:54 +0100

    play:display a game played by the agent trained by train.py

--- reinforcement_learning/0x01-deep_q_learning/play.py
+++ reinforcement_learning/0x01-deep_q_learning/play.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+
+import gym
+import keras
+
+from keras.optimizers import Adam
+
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import GreedyQPolicy
+env = gym.make('BreakoutNoFrameskip-v4')
+env.reset()
+DQN_agent = DQNAgent(
+    model=keras.models.load_model('policy.h5'),
+    nb_actions=env.action_space.n,
+    memory=SequentialMemory(
+        limit=1000000,
+        window_length=4
+    ),
+    policy=GreedyQPolicy()
+)
+DQN_agent.compile(
+    optimizer=Adam(
+        lr=.00025,
+        clipnorm=1.0
+    ),
+    metrics=['mae']
+)
